459ab7fddc63549261119d4c4cb168bf92d2b688
m-krebs <=>
2023-04-20 16:17:52 +0200

    :fire: remove unnecessary comments

--- src/index.js
+++ src/index.js
@@ -1,7 +1,3 @@
-// HELP: https://stackoverflow.com/questions/33144234/chrome-extension-geturl-not-working-in-injected-file
-
-// if you checked "fancy-settings" in extensionizr.com, uncomment this lines
-
 chrome.tabs.query({ active: true }, tabs => {
   const url = tabs[0].url;
   new QRCode(document.getElementById("qrcode"), {
