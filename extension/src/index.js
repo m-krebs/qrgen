5d6559bae4b9b860eea159ab93de8527f866d827
Michael Krebs <=>
2023-06-05 23:06:49 +0200

    feat: make image size autoscale

--- extension/src/index.js
+++ extension/src/index.js
@@ -1,9 +1,16 @@
 chrome.tabs.query({ active: true }, tabs => {
-  const url = tabs[0].url;
+	const url = tabs[0].url;
+	const size = (() => { 
+		const length = url.length;
+		if (length>60) {
+			if (length>150) return 300;
+			return length*2;
+		} else return 120 
+	})();
   new QRCode(document.getElementById("qrcode"), {
     text: url,
-    width: 128,
-    height: 128,
+    width: size,
+    height: size,
     colorDark: "#000000",
     colorLight: "#ffffff",
     correctLevel: QRCode.CorrectLevel.H
